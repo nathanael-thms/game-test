97e8152cb09d3878ba3e62b24564ebc89053340f
Nathanael Thomas <nathanael@thms.uk>
2023-06-30 15:52:40 +0100

    functionality for left,up,right,down and other key added

--- js.js
+++ js.js
@@ -33,6 +33,30 @@ function resetGame() {
 function startsprites() {
     window.addEventListener("keydown",eventfuncton);
 }
-function eventfuncton(){
-    console.log("key has been pressed");
+function eventfuncton(event){
+    //enter key:
+    if (event.keyCode == 13) {
+        console.log("enter key has been presed")
+    }
+    //down key:
+   else if (event.keyCode == 40) {
+        console.log("down key has been pressed");
+    }
+    //up key:
+    else if (event.keyCode == 38) {
+        console.log("up key has been pressed");
+    }
+    //left key:
+    else if (event.keyCode == 37) {
+        console.log("left key has been pressed");
+    }
+    //right key:
+    else if (event.keyCode == 39) {
+        console.log("right key has been pressed");
+    }
+    // other keys:
+    else {
+        console.log("please press either up, down, left, right or enter");
+    }
+    
 }
